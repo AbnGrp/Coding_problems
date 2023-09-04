fbafc1445259697a25e72322fa743f20c96f19c0
Abngrp <abngrp1829@gmail.com>
2023-09-04 10:52:12 -0700

    Actual solution

--- test.py
+++ test.py
@@ -11,3 +11,14 @@ def find_employees(df):
         return l
 
 '''
+'''
+import pandas as pd
+
+def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
+    for i in range(len(employee)):
+        if employee.loc[i,"managerId"]==None:
+            pass
+        else:
+            if int(employee.loc[i,"salary"])>int(employee.loc[int(employee.loc[i,"managerId"])-1,"salary"]):
+               return employee[employee["name"]==employee.loc[i,"name"]]
+'''
