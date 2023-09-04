ce3532714d33fe75ee6eac4338deb292d66975a6
Abngrp <abngrp1829@gmail.com>
2023-09-04 10:31:49 -0700

    Modification of the first problem

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-import pandas as pd
+#https://leetcode.com/problems/employees-earning-more-than-their-managers/description/?lang=pythondata
+'''
+def find_employees(df):
+    l=[]
+    for i in range(len(employee)):
+        if employee.loc[i,"managerId"]==None:
+            pass
+        else:
+            if int(employee.loc[i,"salary"])>int(employee.loc[int(employee.loc[i,"managerId"])-1,"salary"]):
+                l.append(employee.loc[i,"name"])
+        return l
 
-def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
-    for i in range(1,len(employee)+1):
-        if employee.loc[i,"salary"]>employee.loc[employee["managerId"],"salary"]:
-            return employee[i,"name"]
+'''
