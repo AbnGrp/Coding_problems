d02b3f6da34527d575c3c2660054d930c04a8265
Abngrp <abngrp1829@gmail.com>
2023-09-18 07:47:37 -0700

    More problems

--- test.py
+++ test.py
@@ -179,3 +179,19 @@ def fix_names(users: pd.DataFrame) -> pd.DataFrame:
 #Invalid emails
 #https://leetcode.com/problems/find-users-with-valid-e-mails/description/?envType=study-plan-v2&envId=30-days-of-pandas&lang=pythondata
 
+""" import pandas as pd
+
+def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    # Use the str.match() method with a regex pattern to find valid emails
+    valid_emails_df = users[users['mail'].str.match(r'^[A-Za-z][A-Za-z0-9_\.\-]*@leetcode(\?com)?\.com$')]
+    
+    return valid_emails_df """
+
+#Pattients with a condition
+#https://leetcode.com/problems/patients-with-a-condition/description/?envType=study-plan-v2&envId=30-days-of-pandas&lang=pythondata
+
+""" import pandas as pd
+
+def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+    flt=patients["conditions"].str.contains(r'\bDIAB1')
+    return patients[flt] """
