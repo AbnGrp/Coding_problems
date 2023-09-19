8b36d3218f2905d4567061cef67a87a1d24aa7a6
Abngrp <abngrp1829@gmail.com>
2023-09-19 10:25:18 -0700

    More problems

--- test.py
+++ test.py
@@ -195,3 +195,13 @@ def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
 def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
     flt=patients["conditions"].str.contains(r'\bDIAB1')
     return patients[flt] """
+
+#Delete duplicate emails
+#https://leetcode.com/problems/delete-duplicate-emails/description/?envType=study-plan-v2&envId=30-days-of-pandas&lang=pythondata
+
+""" import pandas as pd
+
+# Modify Person in place
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+  person.sort_values(by=["id"],ascending=True,inplace=True)
+  person.drop_duplicates(subset=["email"],keep="first",inplace=True) """
