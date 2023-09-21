37991bbd3697a833d992e732c466c315f5d8c353
Abngrp <abngrp1829@gmail.com>
2023-09-21 07:47:04 -0700

    More problems

--- test.py
+++ test.py
@@ -215,3 +215,15 @@ def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
     return pd.melt(
         products, id_vars='product_id', var_name='store', value_name='price'
     ).dropna() """
+
+#Total time spent by each employee
+#https://leetcode.com/problems/find-total-time-spent-by-each-employee/description/?envType=study-plan-v2&envId=30-days-of-pandas&lang=pythondata
+
+""" import pandas as pd
+
+def total_time(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['total_time'] = employees['out_time'] - employees['in_time']
+
+    empl = employees.groupby(by=['emp_id','event_day'],as_index=False).sum()
+
+    return empl[['event_day','emp_id','total_time']].rename(columns={'event_day':'day'}) """
