c1030eed7ad1bd9ea9df706a7b30057a1772d10b
Abngrp <abngrp1829@gmail.com>
2023-09-30 08:08:33 -0700

    More problems

--- test.py
+++ test.py
@@ -309,3 +309,19 @@ def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> 
     new_df=employees.merge(employee_uni,how="left",on="id")
     return new_df[["unique_id","name"]] """
 
+""" return pd.merge(
+        left=pd.merge(
+            students, subjects, how='cross',
+        ).sort_values(
+            by=['student_id', 'subject_name']
+        ),
+        right=examinations.groupby(
+            ['student_id', 'subject_name'],
+        ).agg(
+            attended_exams=('subject_name', 'count')
+        ).reset_index(),
+        how='left',
+        on=['student_id', 'subject_name'],
+    ).fillna(0)[
+        ['student_id', 'student_name', 'subject_name', 'attended_exams']
+    ] """
