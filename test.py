4566552c6814021e370c1f628efc2b2e1d366aea
Abngrp <abngrp1829@gmail.com>
2023-10-16 07:53:40 -0700

    More problems

--- test.py
+++ test.py
@@ -604,3 +604,14 @@ def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) 
     result_df.columns = ['Department','Employee', 'Salary']
     
     return result_df """
+
+#https://leetcode.com/problems/investments-in-2016/description/
+#Investments in 2016
+
+""" import pandas as pd
+
+def find_investments(insurance: pd.DataFrame) -> pd.DataFrame:
+    uniq_lat_lon = insurance.drop_duplicates(subset = ['lat','lon'], keep = False).pid
+    not_uniq_tiv_2015 = insurance.loc[insurance.duplicated(subset = 'tiv_2015', keep=False)].pid
+    df = insurance.loc[insurance.pid.isin(uniq_lat_lon) & insurance.pid.isin(not_uniq_tiv_2015)]
+    return df[['tiv_2016']].sum().to_frame('tiv_2016').round(2) """
